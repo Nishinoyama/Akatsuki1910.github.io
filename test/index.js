8e6fd7aa37da536c0b406194298c537d2886a4d4
nishinoyama <begoingto1414@gmail.com>
2020-01-21 17:58:20 +0900

    全消し！

--- test/index.js
+++
@@ -1,9 +0,0 @@
-/*jshint esversion:6 */
-$("body").on('click',()=>{
-    //$('.uzu-pole').removeClass('uzu-pole-move');
-    $('.uzu-pole').addClass('uzu-pole-clear');
-    $('#loader-bg').fadeOut(1000);
-    $('#loader').fadeOut(500);
-    $('#main-contents').css('display', 'grid');
-    $('.passing').addClass('move');
-});
